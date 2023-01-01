209f54f8265049c607919b545dc2cc975651095f
Tomas Dato Mgebrisvilis <t.d.mgebrisvilis@gmail.com>
2023-01-02 00:52:39 +0200

    add some functionality

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const grid = document.querySelector('#grid');
-const gridWidth = grid.style.width;
+const gridWidth = grid.clientWidth;
 const gridSizeRangeInput = document.querySelector('#gridSizeRangeInput');
-generateSquares() // initial grid
+const gridSizeIndex = document.querySelector('.gridSizeIndex');
+gridSizeIndex.innerText = `${gridSizeRangeInput.value}x${gridSizeRangeInput.value}`; // display current (default) grid size
+const squareColorInput = document.querySelector('#squareColorInput')
+let color = document.querySelector('#squareColorInput').value
 
-gridSizeRangeInput.addEventListener('input', makeNewGrid)
+generateSquares(); // make initial grid
+const squares = document.querySelectorAll('.square');
 
-function makeNewGrid(e){
+// Settings:
+gridSizeRangeInput.addEventListener('input', makeNewGrid); // change grid size
+squareColorInput.addEventListener('input', () => color = squareColorInput.value) // change color
+
+// Drawing:
+squares.forEach(square => square.addEventListener('mousedown', changeSquareColor))
+
+
+function changeSquareColor(e){
+    e.target.style.background = color
+    squares.forEach(square => square.addEventListener('mouseover', (e) => e.target.style.background = color))
+}
+
+function makeNewGrid(){
     grid.replaceChildren(); // remove previous squares
-    generateSquares()
+    generateSquares();
 }
 
 function generateSquares(){
     for (let i = 0; i < gridSizeRangeInput.value * gridSizeRangeInput.value ; i++){
-        const square = document.createElement('div')
-        const squareSize = gridSizeRangeInput.value
-        square.classList.add('square')
+        const square = document.createElement('div');
+        const squareSize = gridSizeRangeInput.value;
+        gridSizeIndex.innerText = `${squareSize}x${squareSize}`;
+        square.classList.add('square');
         grid.appendChild(square);
-        grid.setAttribute('style', `grid-template-columns: repeat(${squareSize}, ${700 / squareSize}px);`)
+        grid.setAttribute('style', `grid-template-columns: repeat(${squareSize}, ${gridWidth / squareSize}px);`);
     }
 }
