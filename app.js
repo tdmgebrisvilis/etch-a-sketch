3f459021599c41293ea528d56b7eb5a414d2833c
Tomas Dato Mgebrisvilis <t.d.mgebrisvilis@gmail.com>
2023-01-02 03:58:37 +0200

    add drawing functionality

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
+const { log, dir } = console
 const grid = document.querySelector('#grid');
 const gridWidth = grid.clientWidth;
 const gridSizeRangeInput = document.querySelector('#gridSizeRangeInput');
 const gridSizeIndex = document.querySelector('.gridSizeIndex');
-gridSizeIndex.innerText = `${gridSizeRangeInput.value}x${gridSizeRangeInput.value}`; // display current (default) grid size
 const squareColorInput = document.querySelector('#squareColorInput')
 let color = document.querySelector('#squareColorInput').value
+let squares = grid.childNodes
 
-generateSquares(); // make initial grid
-const squares = document.querySelectorAll('.square');
+makeNewGrid(); // make initial grid
 
 // Settings:
-gridSizeRangeInput.addEventListener('input', makeNewGrid); // change grid size
+gridSizeRangeInput.addEventListener('input', makeNewGrid, ); // change grid size
 squareColorInput.addEventListener('input', () => color = squareColorInput.value) // change color
-
-// Drawing:
-squares.forEach(square => square.addEventListener('mousedown', changeSquareColor))
-
-
-function changeSquareColor(e){
-    e.target.style.background = color
-    squares.forEach(square => square.addEventListener('mouseover', (e) => e.target.style.background = color))
-}
 
 function makeNewGrid(){
     grid.replaceChildren(); // remove previous squares
-    generateSquares();
-}
-
-function generateSquares(){
     for (let i = 0; i < gridSizeRangeInput.value * gridSizeRangeInput.value ; i++){
         const square = document.createElement('div');
         const squareSize = gridSizeRangeInput.value;
         gridSizeIndex.innerText = `${squareSize}x${squareSize}`;
-        square.classList.add('square');
+        square.classList.add('square', 'nonDrag');
         grid.appendChild(square);
         grid.setAttribute('style', `grid-template-columns: repeat(${squareSize}, ${gridWidth / squareSize}px);`);
     }
+    gridSizeIndex.innerText = `${gridSizeRangeInput.value}x${gridSizeRangeInput.value}`; // display current grid size
+    squares = grid.childNodes
+    squares.forEach(square => square.addEventListener('mousedown', draw)) // outside this function this wont work properly 
+}
+
+function draw(e){
+    changeSquareColor(e)
+    squares.forEach(square => square.addEventListener('mouseenter', changeSquareColor))
+    window.addEventListener('mouseup', () => {
+        squares.forEach(square => square.removeEventListener('mouseenter', changeSquareColor))
+    })
+}
+
+function changeSquareColor (e) {
+    e.target.style.background = color
 }
